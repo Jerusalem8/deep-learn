055fd6dd07f678663307aace4b0fa339ecaa17c4
‘jerusalem <jerusalem@163.com>
2023-04-05 20:29:00 +0800

    课程2笔记代码更新
    Signed-off-by: ‘jerusalem <‘jerusalem@163.com>

---
+++ Algorithm/src/main/java/class02/DeleteGivenValue.java
@@ -0,0 +1,48 @@
+package class02;
+
+/****
+ * @Author: jerusalem
+ * @Description: DeleteGivenValue
+ * 链表中删除给定值
+ * @Date 2023-04-05 20:16
+ *****/
+public class DeleteGivenValue {
+    /***
+     * 单链表结构
+     */
+    public static class Node {
+        public int value;
+        public Node next;
+
+        public Node(int data) {
+            this.value = data;
+        }
+    }
+
+    /***
+     * 删除链表中等于给定值的节点
+     * @param head
+     * @param num
+     * @return
+     */
+    public static Node removeValue(Node head, int num) {
+        //先遍历，直到head来到第一个不需要删的位置
+        while (head != null) {
+            if (head.value != num) {
+                break;
+            }
+            head = head.next;
+        }
+        Node pre = head;
+        Node cur = head;
+        while (cur != null) {
+            if (cur.value == num) {
+                pre.next = cur.next;
+            } else {
+                pre = cur;  //pre指向上一个不等于给定值的节点
+            }
+            cur = cur.next;
+        }
+        return head;
+    }
+}
